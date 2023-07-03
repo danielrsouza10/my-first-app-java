09f332e4c83e7637af87e7f1ca7c029b10ccae07
Daniel Souza <danielrsouza10@gmail.com>
2023-07-03 10:57:20 -0300

    Constantes e variaveis

---
+++ src/daniel/souza/variaveiseconstantes/VariaveisConstantes.java
@@ -0,0 +1,18 @@
+package daniel.souza.variaveiseconstantes;
+
+public class VariaveisConstantes {
+    public static void main(String[] args) {
+        //constantes sao sempres escritas em CAIXA ALTA
+        int variavel = 10;
+        variavel = 20;
+
+        System.out.println(variavel);
+
+        final int CONSTANTE = 10;
+        System.out.println(CONSTANTE);
+
+        // RETORNA ERRO
+        // CONSTANTE = 20;
+
+    }
+}
