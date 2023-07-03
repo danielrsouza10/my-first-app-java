e9bfff9748132b157bd13f3f203f1b1672c4ce32
Daniel Souza <danielrsouza10@gmail.com>
2023-07-03 10:52:31 -0300

    tipos e variaveis

--- src/daniel/souza/segundasemana/SegundaSemana.java
+++ src/daniel/souza/segundasemana/SegundaSemana.java
@@ -1,5 +1,11 @@
 package daniel.souza.segundasemana;
 
 public class SegundaSemana {
-    
+    public static void main(String[] args) {
+        String nome = "Daniel";
+        System.out.println(nome);
+
+        double salarioFuncionario = 2.500;
+        System.out.println(salarioFuncionario);
+    }
 }
