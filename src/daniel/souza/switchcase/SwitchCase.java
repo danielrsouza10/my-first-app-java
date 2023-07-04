3ea9e2b7557ec4c8a7ff2418ad3375a6803bb579
Daniel Souza <danielrsouza10@gmail.com>
2023-07-04 17:27:13 -0300

    pratica de switch case

---
+++ src/daniel/souza/switchcase/SwitchCase.java
@@ -0,0 +1,27 @@
+package daniel.souza.switchcase;
+
+public class SwitchCase {
+    
+    public static void main(String[] args) {
+
+        String sigla = "P";
+
+        switch (sigla) {
+        case "P": {
+            System.out.println("Pequeno");
+            break;
+        }
+        case "M":{
+            System.out.println("Medio");
+            break;
+        }
+        case "G":{
+            System.out.println("Grande");
+            break;
+        }
+        default:
+            System.out.println("Indefinido");;
+    }
+    }
+    
+}
